c08e458a6f6119156c6ad7f3b7b8291d31e83b44
Silas Michael Knight <silasfelinus@gmail.com>
2021-03-08 02:25:54 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(pokemon)
+    @id = pokemon[:id]
+    @name =pokemon[:name]
+    @type = pokemon[:type]
+    @db = pokemon[:db]
+  end
+
+  def self.save(name, type, id)
+
+  end
+
+  def self.find(id)
+  end
 end
