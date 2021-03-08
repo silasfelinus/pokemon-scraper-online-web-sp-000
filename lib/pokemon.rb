5bc07ddb3fcfcac2d3a25c213b4e22b9b2e3f295
Silas Michael Knight <silasfelinus@gmail.com>
2021-03-08 03:37:34 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,8 @@
+require "pry"
+
 class Pokemon
   attr_accessor :id, :name, :type, :db
+
 
   def initialize(pokemon)
     @id = pokemon[:id]
@@ -14,9 +17,18 @@ class Pokemon
       VALUES (?, ?)
     SQL
     db.execute(sql, name, type)
-    @id = db.execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
 
-  def self.find(id)
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+      LIMIT 1
+    SQL
+   new_pokemon = db.execute(sql, id)[0]
+   newest = self.new(:id => new_pokemon[0], :name => new_pokemon[1], :type => new_pokemon[2], :db => db)
+   newest
   end
 end
